5db9793f39f29a164bbb2f235a3a105847448451
weloper <sen.webdev@hotmail.com>
2019-07-03 09:40:28 +0200

    cheapticket demo

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+export default class Home extends Vue {}
+</script>
